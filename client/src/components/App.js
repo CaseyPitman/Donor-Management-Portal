5323616b4186e88072a126236ad9e2816a6a2e2e
Casey The Great <casey@caseypitman.com>
2020-12-28 17:03:04 -0600

    Installed react-bootstrap-icons and tested.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { BookHalf } from "react-bootstrap-icons";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
+      <BookHalf style = {{color: 'red'}}/>
     </div>
   );
 };
