e59ea6bc1999ea32d02526ee219dc74987e4d840
Casey The Great <casey@caseypitman.com>
2020-12-28 17:14:11 -0600

    App: commenting and quote.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { BookHalf } from "react-bootstrap-icons";
+import { Book } from "react-bootstrap-icons";
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
-      <BookHalf style = {{color: 'red'}}/>
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,14 @@
+/*  
+This app enables non-profit organizations and charities to track the names of past donors, 
+including donation history (dates, amounts, and types), and contact information to enable 
+soliciation of future donations. 
+*/
+
+/* 
+"No one has ever become poor by giving."
+                            -Anne Frank
+*/
+
 import React from "react";
 import ReactDOM from "react-dom";
 
