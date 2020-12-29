a5538766a8658cb162cb232ae8d8427c75cae5b4
Casey The Great <casey@caseypitman.com>
2020-12-28 22:37:47 -0600

    Home: added splash image and styles.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,10 +6,17 @@ a login button, a splash image and a quote about the value of literacy.
 //Components
 import Header from "./Header";
 
+//Assets
+import hero from "../images/child-reading.jpg";
+
+//Styles
+import "../css/home.css";
+
 const Home = () => {
   return (
     <div>
       <Header />
+      <img src={hero} alt='Child reading in wonder.' className='hero-image' />
     </div>
   );
 };
