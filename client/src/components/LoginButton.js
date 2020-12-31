d1f8b2274d954c02290e4e32a60df1ea9498113d
Casey The Great <casey@caseypitman.com>
2020-12-30 19:35:04 -0600

    created LoginButton component.

---
+++ client/src/components/LoginButton.js
@@ -0,0 +1,12 @@
+// This component renders a login button linked to Auth0 universal login page.
+
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Sign In</button>;
+};
+
+export default LoginButton;
