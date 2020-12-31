9dd19a5ef69edf4456de4b449ffbe73664d0e760
Casey The Great <casey@caseypitman.com>
2020-12-31 11:27:26 -0600

    Header: LogoutButton styles and hover styles.

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,13 +1,21 @@
+/* This component renders the logout button.  */
+
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+
+//Components
+import Button from "./Button";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
-      Log Out
-    </button>
+    <Button
+      btnColor='dark-button'
+      btnSize='medium-button'
+      btnText='Sign Out'
+      onClick={() => logout({ returnTo: window.location.origin })}
+    />
   );
 };
 
