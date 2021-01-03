9bfdbc416706cbdfc7949aefbd6035448c266cfa
Casey The Great <casey@caseypitman.com>
2021-01-02 18:02:10 -0600

    actions/types.js holds variables for names of each actions.

---
+++ client/src/actions/types.js
@@ -0,0 +1,7 @@
+// Creates variables for actions
+
+export const FETCH_DONOR_LIST;
+export const CREATE_DONOR;
+export const FETCH_DONOR_DETAILS;
+export const EDIT_DONOR;
+export const DELETE_DONOR;
