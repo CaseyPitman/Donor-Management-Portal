b9e2cdb0e443cd2e84e1dea9fb0ceb0853b18cd6
Casey The Great <casey@caseypitman.com>
2021-01-11 16:02:36 -0600

    DonorList: completed form schema. Material-ui base styles. No custom styles.

--- client/src/components/Donors/DonorForm.js
+++ client/src/components/Donors/DonorForm.js
@@ -3,70 +3,89 @@
 
 import React from "react";
 
-import Form from "@rjsf/core";
-
+// import Form from "@rjsf/core";
+import Form from "@rjsf/material-ui";
 
 class DonorForm extends React.Component {
+  schema = {
+    type: "object",
+    required: ["firstName", "lastName", "email", "phone"],
+    properties: {
+      firstName: {
+        type: "string",
+        title: "First Name",
+      },
+      lastName: {
+        type: "string",
+        title: "Last Name",
+      },
+      organization: {
+        type: "string",
+        title: "Organization",
+      },
+      email: {
+        type: "string",
+        title: "Email",
+      },
+      phone: {
+        type: "string",
+        title: "Phone",
+      },
+      street: {
+        type: "string",
+        title: "Street",
+      },
+      city: {
+        type: "string",
+        title: "City",
+      },
+      state: {
+        type: "string",
+        title: "State",
+        enum: ["a", "b", "c"],
+        enumNames: ["aligator", "bear", "cat"],
+      },
+      donations: {
+        type: "array",
+        title: "Donation History",
+        items: {
+          type: "object",
+          properties: {
+            date: { type: "string", title: "Date" },
+            amount: { type: "number", title: "Amount" },
+            type: {
+              type: "string",
+              title: "Type",
+              enum: ["gift", "endowment", "scholarship"],
+              enumNames: ["Gift", "Endowment", "Scholarship"],
+            },
+          },
+        },
+      },
+    },
+  };
 
-schema = {
-  type: "object",
-  required: ["firstName", "lastName", "email", "phone"],
-  properties: {
-    firstName: {
-      type: "string",
-      title: "First Name",
+  uiSchema = {
+    properties: {
+      state: {
+        "ui:widget": "select",
+      },
     },
-    lastName: {
-      type: "string",
-      title: "Last Name",
-    },
-    organization: {
-      type: "string",
-      title: "Organization",
-    },
-    email: {
-      type: "string",
-      title: "Email",
-    },
-    phone: {
-      type: "string",
-      title: "Phone",
-    },
-    street: {
-      type: "string",
-      title: "Street",
-    },
-    city: {
-      type: "string",
-      title: "City",
-    },
-    state: {
-      type: "string",
-      title: "State",
-      enum: ['a', 'b', 'c'], 
-      enumNames: ['aligator', 'bear', 'cat']
-    },
-  },
-};
-
-uiSchema = {
-  state: {
-    "ui:widget": "select", // could also be "select"
-  },
-};
-
-
+  };
 
   //for testing
 
-  onSubmit = ({formData}) => {
-    console.log(formData)
+  onSubmit = ({ formData }) => {
+    console.log(formData);
   };
 
-
-
   render() {
-    return <Form schema={this.schema} uiSchema={this.uiSchema} onSubmit = {this.onSubmit}></Form>;
+    return (
+      <Form
+        schema={this.schema}
+        uiSchema={this.uiSchema}
+        onSubmit={this.onSubmit}></Form>
+    );
   }
 }
 
