74e6d6126bc3a1dc0ff2828df8c1dd5379068488
Casey The Great <casey@caseypitman.com>
2021-01-11 13:28:07 -0600

    installed react-jsonschema-form. Started building schema.

--- client/src/components/Donors/DonorForm.js
+++ client/src/components/Donors/DonorForm.js
@@ -2,93 +2,34 @@
 // updating an existing record.
 
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 
+import Form from "@rjsf/core";
+
+const schema = {
+  type: "object",
+  required: ["firstName"],
+  properties: {
+    firstName: {
+      type: "string",
+      title: "First Name",
+    },
+    lastName: {
+      type: "string",
+      title: "Last Name",
+    },
+  },
+};
 
 class DonorForm extends React.Component {
-
   //for testing
 
-  onSubmit = () => {
-    console.log("form submitted")
-  }
+  onSubmit = () => {};
 
-
-
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className='ui error message'>
-          <div className='header'>{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete='off' />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
-  renderSelectField = () => {
-    return (
-      <select>
-        <option value='blah'>Blah</option>
-      </select>
-    );
-  };
-
-  renderTextField = () => {
-    return (<input></input>)
-  }
-
-  handleSubmit = formValues => {
-    this.props.onSubmit(formValues);
-  };
+  handleSubmit = formValues => {};
 
   render() {
-    return (
-      <form
-        className='ui form error'
-        onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <Field name='firstName' component={this.renderInput} label='First Name' />
-        <Field
-          name='lastName'
-          component={this.renderInput}
-          label='Last Name'
-        />
-        <Field
-          name='state'
-          component={this.renderSelectField}
-          label='State'
-        />
-        <button className='ui button primary'>Submit</button>
-      </form>
-    );
+    return <Form schema={schema}></Form>;
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-  if (!formValues.firstName) {
-    //only run if user did not enter a title
-    errors.title = "You must enter a title.";
-  }
-  if (!formValues.LastName) {
-    //only run if user did enter a description
-    errors.description = "You must enter a description.";
-  }
-  return errors;
-};
-
-export default reduxForm({
-  form: "donorForm",
-  validate: validate,
-})(DonorForm);
+export default DonorForm;
