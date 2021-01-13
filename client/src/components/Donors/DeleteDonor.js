58d483bd1cb78229060b79d7bd616b09d69a478d
Casey The Great <casey@caseypitman.com>
2021-01-13 14:35:23 -0600

    Converted DeleteDonor to class based component in order to access ownProps.

--- client/src/components/Donors/DeleteDonor.js
+++ client/src/components/Donors/DeleteDonor.js
@@ -5,24 +5,14 @@ delete a record of a donor.
 
 import React from "react";
 
-//Hooks
-import { useAuth0 } from "@auth0/auth0-react";
-import { useHistory } from "react-router-dom";
-
-const DeleteDonor = () => {
-  const { isAuthenticated, isLoading } = useAuth0();
-  const history = useHistory();
-
-  if (!isAuthenticated && !isLoading) {
-    history.push("/");
+class DeleteDonor extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Delete Donor</h1>
+      </div>
+    );
   }
-  return (
-    <div>
-      <h1>Delete Donor</h1>
-    </div>
-  );
-};
+}
 
 export default DeleteDonor;
-
-
