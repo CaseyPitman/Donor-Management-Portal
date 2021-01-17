a8b6a125545f18379eb3c16bd5e9379a60916679
Casey The Great <casey@caseypitman.com>
2021-01-17 09:47:23 -0600

    Modal: import MUI components/styles.

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,7 +1,15 @@
 //This module renders the modal dialog used when deleting a record.
 
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
+
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const Modal = props => {
   return ReactDOM.createPortal(
