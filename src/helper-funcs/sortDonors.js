3be78d162f09dd1d0c7a8c65514a3e3c0f794215
Casey The Great <casey@caseypitman.com>
2021-01-27 11:01:21 -0600

    Created sortDonors helper-func, no logic yet.

---
+++ src/helper-funcs/sortDonors.js
@@ -0,0 +1,11 @@
+/* This function will take a list of donors and sort them based on chosen 
+criteria - alphabetical or total donation amount */
+
+const sortDonors = () => {
+
+
+
+   return "sorted donors list"
+}
+
+export default sortDonors;
