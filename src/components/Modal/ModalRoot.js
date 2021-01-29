0e5b0518354118ffe2438d5d50254db735c7cfd4
Casey The Great <casey@caseypitman.com>
2021-01-29 14:26:07 -0600

    Created Modal container class based component.

---
+++ src/components/Modal/ModalRoot.js
@@ -0,0 +1,18 @@
+/* This component renders the modal container and connects to redux store. */
+
+import React from "react";
+import { connect } from "react-redux";
+import ReactModal from "react-modal";
+
+
+class ModalContainer extends React.Component {
+   
+   
+   render(){
+      return
+   
+   
+   }
+
+
+}
