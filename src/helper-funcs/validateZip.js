a023d0039a4ef491ba23f63e10932d7560aca3b5
Casey The Great <casey@caseypitman.com>
2021-02-07 11:05:57 -0600

    FIX: clean up and comment.

--- src/helper-funcs/validateZip.js
+++ src/helper-funcs/validateZip.js
@@ -1,10 +1,11 @@
 //Helper function to validate zip codes and limit to 5 digits.
 
 const validateZip = input => {
+  //No current input return as is
   if (!input) return input;
-
+  //Only number inputs.
   const zip = input.replace(/[^\d]/g, "");
-
+  //Limit to 5 digits.
   if (zip.length < 5) {
     return zip;
   }
