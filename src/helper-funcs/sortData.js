b42ad49db744467dfbee0d0befe4d83e289b5964
Casey The Great <casey@caseypitman.com>
2021-02-06 19:18:36 -0600

    FIX: clean up and commenting.

--- src/helper-funcs/sortData.js
+++ src/helper-funcs/sortData.js
@@ -1,20 +1,21 @@
-/* This function will take a list of donors and sort them based on chosen 
-criteria - alphabetical or total donation amount */
+/* 
+This function takes data, a dataType (donor list or donations), and sortBy (a method by which
+to sort - i.e. alphabetically,  numerically, etc)
+ */
 
 const sortData = (data, dataType, sortBy) => {
+  //Store sorted data.
   let sortedData = [];
 
+  //USER WISHES TO SORT THE FULL LIST OF DONORS
   if (dataType === "donor list") {
-    //User wishes to sort full list of donors
-
-    // Sort by total donations
+    // Sort donor list by total donations
     if (sortBy === "totalDonations") {
       sortedData = data.sort((a, b) => {
         return b.totalDonations - a.totalDonations;
       });
     }
-
-    //Sort Alphabetically
+    //Sort donor list alphabetically
     else {
       sortedData = data.sort((a, b) => {
         const nameA = a.fullName.toUpperCase();
@@ -26,23 +27,24 @@ const sortData = (data, dataType, sortBy) => {
         if (nameA > nameB) {
           return 1;
         }
-
         //Names Are Equal
         return 0;
       });
     }
-  } else if (dataType === "donations") {
-    //User wishes to sort list of single donor's donations.
+  }
+  //USER WISHES TO SORT INDIVIDUAL DONOR'S DONATION HISTORY
+  else if (dataType === "donations") {
+    //Sort donations by donation amount descending
     if (sortBy === "donation amount") {
       sortedData = data.sort((a, b) => {
         return b.amount - a.amount;
       });
-    } else if (sortBy === "donation type") {
-      //Sort by donation type
+    }
+    //Sort donations by donation type.
+    else if (sortBy === "donation type") {
       sortedData = data.sort((a, b) => {
         const typeA = a.type.toUpperCase();
         const typeB = b.type.toUpperCase();
-
         if (typeA < typeB) {
           return -1;
         }
@@ -52,14 +54,16 @@ const sortData = (data, dataType, sortBy) => {
         //Same types
         return 0;
       });
-    } else if (sortBy === "date ascending") {
-      //Oldest Date First
+    }
+    //Sort donations by data, oldest to newest.
+    else if (sortBy === "date ascending") {
       sortedData = data.sort((a, b) => {
         //Use regex to remove dashes from the date during sort.
         return a.date.replace(/-/g, "") - b.date.replace(/-/g, "");
       });
-    } else {
-      //Newest date first - default
+    }
+    //Sort donations by data, newest to oldest
+    else {
       sortedData = data.sort((a, b) => {
         //Use regex to remove dashes from the date during sort.
         return b.date.replace(/-/g, "") - a.date.replace(/-/g, "");
