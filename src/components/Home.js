0a715b5f2f988c1ebb2abec5705bf01289a4d0e0
Casey The Great <casey@caseypitman.com>
2021-02-07 10:42:41 -0600

    FIX: clean up and comment.

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,7 @@ import { useHistory } from "react-router-dom";
 import "../css/home.css";
 
 const Home = () => {
+  //Dertermine auth status
   const { isAuthenticated } = useAuth0();
   const history = useHistory();
 
@@ -20,27 +21,25 @@ const Home = () => {
   if (isAuthenticated) {
     history.push("/donor-list");
     return <div></div>;
-  } 
-  
+  }
 
-    return (
-      <div className='home'>
-        <div className='quote-container'>
-          <h1 className='quote'>
-            <i className='fas fa-quote-left quotation-mark'></i>
-            The more that you read,
-            <br></br>
-            the more things you will know.
-            <br></br>
-            The more that you learn,
-            <br></br>
-            the more places you’ll go.
-          </h1>
-          <h2 className='quote-attribution'>- Dr. Seuss</h2>
-        </div>
+  return (
+    <div className='home'>
+      <div className='quote-container'>
+        <h1 className='quote'>
+          <i className='fas fa-quote-left quotation-mark'></i>
+          The more that you read,
+          <br></br>
+          the more things you will know.
+          <br></br>
+          The more that you learn,
+          <br></br>
+          the more places you’ll go.
+        </h1>
+        <h2 className='quote-attribution'>- Dr. Seuss</h2>
       </div>
-    );
-
+    </div>
+  );
 };
 
 export default Home;
